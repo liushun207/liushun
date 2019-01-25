285903f39acf2a7e961c9f6adf79097b448c22e8
刘顺 <642517492@qq.com>
2019-01-25 18:04:18 +0800

    新增测试文件

---
+++ src/main/java/com/concurrent/Test.java
@@ -0,0 +1,58 @@
+package com.concurrent;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+
+/**
+ * 多线程测试.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        // 设置线程名称
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("demo-pool-%d").build();
+
+        // 创建 核心线程数 5  最大线程数 10  缓存线程数 5 的线程池
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10,
+                200, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(5),
+                namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        // 当线程大于15个的时候 就会抛出任务拒绝异常了
+        for(int i = 0; i < 15; i++)
+        {
+            MyTask myTask = new MyTask(i);
+            executor.execute(myTask);
+            System.out.println("线程池中线程数目：" + executor.getPoolSize() + "，队列中等待执行的任务数目：" + executor.getQueue().size() + "，已执行完的任务数目：" + executor.getCompletedTaskCount());
+        }
+
+        executor.shutdown();
+    }
+}
+
+class MyTask implements Runnable
+{
+    private int taskNum;
+
+    public MyTask(int num)
+    {
+        this.taskNum = num;
+    }
+
+    @Override
+    public void run()
+    {
+        System.out.println("正在执行task " + taskNum + " - " + Thread.currentThread().getName());
+        try
+        {
+            Thread.currentThread().sleep(4000);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("task " + taskNum + "执行完毕");
+    }
+}
