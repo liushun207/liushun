8f10c0be11d0d577458ed8acf0d33ef33b3ecbf7
刘顺 <642517492@qq.com>
2019-01-26 11:42:28 +0800

    注释补充

--- src/main/java/com/util/ThreadPoolService.java
+++ src/main/java/com/util/ThreadPoolService.java
@@ -44,7 +44,7 @@ public class ThreadPoolService
     // region 定时任务
 
     /**
-     * 计划任务线程
+     * 计划任务线程,需要控制定时任务的线程数
      */
     private static ScheduledExecutorService schedule = new ScheduledThreadPoolExecutor(5,
             new BasicThreadFactory.Builder().namingPattern("liushun-schedule-pool-%d").daemon(true).build());
@@ -67,6 +67,7 @@ public class ThreadPoolService
 
     /**
      * 取决于每次任务执行的时间长短，是基于不固定时间间隔进行任务调度.
+     * 以上一次任务的结束时间计算下一次任务的开始时间
      * @param command 要执行的任务
      * @param initialDelay 初始延迟
      * @param delay 延迟时间（上次任务完成结束后的时间 再延迟 delay 执行）
